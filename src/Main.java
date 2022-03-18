bca92a68777ca708ea6409ea18e26f1983053692
leesg619 <gkwoap@nate.com>
2022-03-19 00:28:40 +0900

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main {
+    static int num=0;
+    static ArrayList<Integer> answer = new ArrayList<>();
+    static int[][] ch;
+
+
+    public void solution() {
+
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+
+        T.solution();
+
+    }
+}
+
+// for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
